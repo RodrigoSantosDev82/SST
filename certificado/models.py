1ff3734bd1edf327ec845f19aeb48cc723df7f69
RodrigoSantosDev82 <166881134+RodrigoSantosDev82@users.noreply.github.com>
2024-12-27 14:43:17 -0300

    Update models.py

--- certificado/models.py
+++ certificado/models.py
@@ -9,7 +9,7 @@ import os
 class Certificado(models.Model):
     nome = models.CharField(max_length=255)  # Nome do Certificado
     validade_meses = models.IntegerField()  # Validade em meses
-    colaborador_id = models.CharField(max_length=255, null=True)
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.nome} - {self.validade_meses} meses"
